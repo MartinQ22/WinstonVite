68ef820ded2754257d8a09a820c18e71d9c8fa37
Martin Quiroga <112977790+MartinQ22@users.noreply.github.com>
2025-06-02 06:21:09 -0300

    v1.12 error deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
 import { useState } from 'react'
 import './App.css'
-import NavBar from './components/NavBar'
-import ItemListContainer from './components/ItemListContainer'
+import CartWidget from './components/CartWidget'
 
 function App() {
   
   return(
     <>
     {/* Navegacion */}
-    <NavBar />
+    <nav className='NavBar'>
+      {/* Logo */}
+      <div className='winstonLogo'>
+        <img className='logoImg' src="https://res.cloudinary.com/dfwb8a17z/image/upload/v1748842997/WinstonLogo_adtxyy.jpg" alt="logo mono" />
+        <h3>Winston</h3>
+      </div>
+      {/* Links Nav */}
+      <ul className='navLinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Ofertas</a></li>
+        <li><a href="#">Productos</a></li>
+        <li><a href="#">Contacto</a></li>
+        <li><a href="#">Mis Pedidos</a></li>     
+      </ul>
+      {/* CartWidth */}
+      <CartWidget />
+    </nav>
     {/* Main */}
     <main>
-      {/* Lista de productos */}
-      <ItemListContainer text="Bienvenido a Winston!" />
+      <div className='itemListContainer'>
+        
+      </div>
     </main>
 
     </>
