1c94766af93006ce92170ef7c17d189382b5cfe7
Martin Quiroga <112977790+MartinQ22@users.noreply.github.com>
2025-06-02 05:44:11 -0300

    Pre-entrega 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
 import { useState } from 'react'
 import './App.css'
-import CartWidget from './components/CartWidget'
+import NavBar from './components/NavBar'
+import ItemListContainer from './components/ItemListContainer'
 
 function App() {
   
   return(
     <>
     {/* Navegacion */}
-    <nav className='NavBar'>
-      {/* Logo */}
-      <div className='winstonLogo'>
-        <img className='logoImg' src="https://res.cloudinary.com/dfwb8a17z/image/upload/v1748842997/WinstonLogo_adtxyy.jpg" alt="logo mono" />
-        <h3>Winston</h3>
-      </div>
-      {/* Links Nav */}
-      <ul className='navLinks'>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Ofertas</a></li>
-        <li><a href="#">Productos</a></li>
-        <li><a href="#">Contacto</a></li>
-        <li><a href="#">Mis Pedidos</a></li>     
-      </ul>
-      <CartWidget />
-    </nav>
+    <NavBar />
     {/* Main */}
     <main>
-      <div className='itemListContainer'>
-        
-      </div>
+      {/* Lista de productos */}
+      <ItemListContainer text="Bienvenido a Winston!" />
     </main>
 
     </>
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,11 @@
 function Logo () {
 
     return (
-        <button className='cartWidget' onClick={handleClick}>
-            <img src="https://res.cloudinary.com/dfwb8a17z/image/upload/v1748839992/cart-8-svgrepo-com_i2laqa.png" alt="icono carrito" />
-            <p className='cartContador'>{cantidadItems}</p>
-        </button>
+        <div className='winstonLogo'>
+        <img className='logoImg' src="https://res.cloudinary.com/dfwb8a17z/image/upload/v1748842997/WinstonLogo_adtxyy.jpg" alt="logo mono" />
+        <h3>Winston</h3>
+      </div>
     )
 }
+
+export default Logo;
